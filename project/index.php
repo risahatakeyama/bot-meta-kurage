9e3fcd89e0c5ebd454c39e6562e9a39fabc106b8
risahatakeyama <0frt389es2pofty@gmail.com>
2020-12-06 15:32:19 +0900

    c

--- project/index.php
+++ project/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/bot-meta-kurage/projcet/vender/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 $httpClient=new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
 
