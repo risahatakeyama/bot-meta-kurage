9cba62391ba88c18c468e86944d0f08a858e7bfb
risahatakeyama <0frt389es2pofty@gmail.com>
2020-12-06 14:07:12 +0900

    coommit

--- project/index.php
+++ project/index.php
@@ -2,7 +2,16 @@
 
 require_once QDIRQ . '/vender/autoload.php';
 
-$inputString=file_get_contents('php://input');
-error_log($inputString);
+$httpClient=new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+
+$bot=new \LINE\LINEBot($httpClient,['channelSecret'=>getenv('CHANNEL_SECRET')]);
+
+$signature=$_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+
+$events=$bot->parseEventRequest(file_get_contents('php://input'),$signature);
+
+foreach($events as $event){
+	$bot->replayText($event->getReplayToken(),'TextMessage');
+}
 
 ?>
